45cdade923db043d4402289456c0086a597d1b11
Phoomparin Mano <work@phoom.in.th>
2017-07-22 14:42:15 +0700

    :tada: Off to a completely fresh start!

--- api/src/constants/util.js
+++
@@ -1,3 +0,0 @@
-export const IS_CLIENT = (typeof window !== "undefined")
-export const IS_DEV = (process.env.NODE_ENV === "development")
-export const IS_PROD = (process.env.NODE_ENV === "production")
