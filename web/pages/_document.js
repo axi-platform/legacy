17cb265907a855008afc0ae8181b63181952bbe1
Pmc Dev <phoomparin@gmail.com>
2017-09-02 01:40:15 +0700

    :wrench: Linter Fixes and Minor Refactorings

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -6,35 +6,36 @@ import {flush} from 'emotion'
 const dev = process.env.NODE_ENV !== 'production'
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
+  static getInitialProps({renderPage}) {
     if (dev) {
       flush()
     }
+
     const page = renderPage()
     const styles = extractCritical(page.html)
-    return { ...page, ...styles }
+
+    return {...page, ...styles}
   }
 
-  constructor (props) {
+  constructor(props) {
     super(props)
-    const { __NEXT_DATA__, ids } = props
+
+    const {__NEXT_DATA__, ids} = props
     if (ids) {
       __NEXT_DATA__.ids = ids
     }
   }
 
-  render () {
-    return (
-      <html>
-        <Head>
-          <title>With Emotion</title>
-          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    )
-  }
+  render = () => (
+    <html lang='en'>
+      <Head>
+        <title>Axi Dashboard</title>
+        <style dangerouslySetInnerHTML={{__html: this.props.css}} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  )
 }
